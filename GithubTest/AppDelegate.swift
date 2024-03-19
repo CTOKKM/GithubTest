ccec1a7de62bcc209afdf2f1096faae5550a22ad
CTOKKM <gkzl95@naver.com>
2024-03-20 00:27:57 +0900

    Initial Commit

---
+++ GithubTest/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  GithubTest
+//
+//  Created by KKM on 3/20/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ GithubTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GithubTest
+//
+//  Created by KKM on 3/20/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
